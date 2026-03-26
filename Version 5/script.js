a4db0b20adcf97ae2e5d69bec08810ad2196f883
FluxDev0 <felix.b.m.henrichs@gmail.com>
2026-03-26 20:18:55 +0100

    big and small changes
    added variables to .css file for better maintainability. some flex stuff for everything and better gap and margin management. also better bars and some random changes

--- Version 5/script.js
+++ Version 5/script.js
@@ -9,8 +9,8 @@ function showTab(tab) {
   if (tab === "home") {
     placeholder.innerHTML = `
       <div class="info glass" id="content">
-      <p>Die Planet Health Diet ist gut für dich und die Umwelt.</p>
-      <p>Ziel: Nachhaltig und gesund essen.</p>
+        <h5>Eine Website über die Planetary Health Diet.</h5>
+        <h6>ohne KI.</h6>
       </div>
     `;
   }
@@ -18,7 +18,6 @@ function showTab(tab) {
   if (tab === "info") {
     placeholder.innerHTML = `
       <div class="info glass" id="content">
-      <h2>Was bedeutet das?</h2>
       <ul>
         <li>Weniger Fleisch</li>
         <li>Mehr Gemüse</li>
@@ -178,61 +177,62 @@ document.querySelectorAll("nav.mnav button").forEach((box, index) => {
     document.querySelectorAll("nav.mnav button").forEach(b => b.classList.remove("active"));
     this.classList.add("active");
     navindication("mnav", index);
+    showTab(this.id);
   });
 });
 
 const phd = [
-  { value: 300, color: "rgba(76, 175, 80, 0.8)", label: "Gemüse" },
-  { value: 250, color: "rgba(255, 152, 0, 0.8)", label: "Milchprodukte" },
+  { value: 300, color: "rgba(70, 184, 74, 0.8)", label: "Gemüse" },
+  { value: 250, color: "rgba(255, 196, 0, 0.81)", label: "Milchprodukte" },
   { value: 232, color: "rgba(33, 150, 243, 0.8)", label: "Vollkornprodukte" },
   { value: 200, color: "rgba(244, 67, 54, 0.8)", label: "Obst" },
-  { value: 75, color: "rgba(156, 39, 176, 0.8)", label: "Hülsenfrüchte" },
-  { value: 50, color: "rgba(63, 218, 33, 0.8)", label: "Wurzelgemüse" },
-  { value: 50, color: "rgba(244, 54, 212, 0.8)", label: "Nüsse & Samen" },
-  { value: 40, color: "rgba(244, 67, 54, 0.8)", label: "Pflanzenöle" },
-  { value: 31, color: "rgba(244, 231, 54, 0.8)", label: "Zucker" },
-  { value: 29, color: "rgba(54, 244, 235, 0.8)", label: "Geflügel" },
-  { value: 28, color: "rgba(0, 0, 0, 0.8)", label: "Fisch" },
-  { value: 14, color: "rgba(244, 67, 54, 0.8)", label: "rotes Fleisch" },
-  { value: 13, color: "rgba(36, 95, 9, 0.8)", label: "Eier" },
-  { value: 7, color: "rgba(60, 244, 54, 0.8)", label: "Palmöl" },
-  { value: 5, color: "rgba(236, 61, 45, 0.8)", label: "tierisches Fett" },
+  { value: 75, color: "rgba(188, 30, 216, 0.8)", label: "Hülsenfrüchte" },
+  { value: 50, color: "rgba(70, 184, 74, 0.8)", label: "Wurzelgemüse" },
+  { value: 50, color: "rgba(255, 196, 0, 0.81)", label: "Nüsse & Samen" },
+  { value: 40, color: "rgba(33, 150, 243, 0.8)", label: "Pflanzenöle" },
+  { value: 31, color: "rgba(244, 67, 54, 0.8)", label: "Zucker" },
+  { value: 29, color: "rgba(188, 30, 216, 0.8)", label: "Geflügel" },
+  { value: 28, color: "rgba(70, 184, 74, 0.8)", label: "Fisch" },
+  { value: 14, color: "rgba(255, 196, 0, 0.81)", label: "rotes Fleisch" },
+  { value: 13, color: "rgba(33, 150, 243, 0.8)", label: "Eier" },
+  { value: 7, color: "rgba(244, 67, 54, 0.8)", label: "Palmöl" },
+  { value: 5, color: "rgba(188, 30, 216, 0.8)", label: "tierisches Fett" },
 ];
 
 const north_america = [
-  { value: 203, color: "#4CAF50", label: "Gemüse" },
-  { value: 472, color: "#FF9800", label: "Milchprodukte" },
-  { value: 39, color: "#2196F3", label: "Vollkornprodukte" },
-  { value: 159, color: "#F44336", label: "Obst" },
-  { value: 15, color: "#9C27B0", label: "Hülsenfrüchte" },
-  { value: 102, color: "#3fda21", label: "Wurzelgemüse" },
-  { value: 23, color: "#f436d4", label: "Nüsse & Samen" },
-  { value: 52, color: "#F44336", label: "Pflanzenöle" },
-  { value: 89, color: "#f4e736", label: "Zucker" },
-  { value: 99, color: "#36f4eb", label: "Geflügel" },
-  { value: 26, color: "#000000", label: "Fisch" },
-  { value: 103, color: "#F44336", label: "rotes Fleisch" },
-  { value: 31, color: "#245f09", label: "Eier" },
-  { value: 1, color: "#3cf436", label: "Palmöl" },
-  { value: 10, color: "#ec3d2d", label: "tierisches Fett" },
+  { value: 203, label: "Gemüse" },
+  { value: 472, label: "Milchprodukte" },
+  { value: 39, label: "Vollkornprodukte" },
+  { value: 159, label: "Obst" },
+  { value: 15, label: "Hülsenfrüchte" },
+  { value: 102, label: "Wurzelgemüse" },
+  { value: 23, label: "Nüsse & Samen" },
+  { value: 52, label: "Pflanzenöle" },
+  { value: 89, label: "Zucker" },
+  { value: 99, label: "Geflügel" },
+  { value: 26, label: "Fisch" },
+  { value: 103, label: "rotes Fleisch" },
+  { value: 31, label: "Eier" },
+  { value: 1, label: "Palmöl" },
+  { value: 10, label: "tierisches Fett" },
 ];
 
 const europe = [
-  { value: 235, color: "#4CAF50", label: "Gemüse" },
-  { value: 489, color: "#FF9800", label: "Milchprodukte" },
-  { value: 82, color: "#2196F3", label: "Vollkornprodukte" },
-  { value: 164, color: "#F44336", label: "Obst" },
-  { value: 11, color: "#9C27B0", label: "Hülsenfrüchte" },
-  { value: 155, color: "#3fda21", label: "Wurzelgemüse" },
-  { value: 12, color: "#f436d4", label: "Nüsse & Samen" },
-  { value: 44, color: "#F44336", label: "Pflanzenöle" },
-  { value: 69, color: "#f4e736", label: "Zucker" },
-  { value: 46, color: "#36f4eb", label: "Geflügel" },
-  { value: 27, color: "#000000", label: "Fisch" },
-  { value: 83, color: "#F44336", label: "rotes Fleisch" },
-  { value: 31, color: "#245f09", label: "Eier" },
-  { value: 3, color: "#3cf436", label: "Palmöl" },
-  { value: 21, color: "#ec3d2d", label: "tierisches Fett" },
+  { value: 235, label: "Gemüse" },
+  { value: 489, label: "Milchprodukte" },
+  { value: 82, label: "Vollkornprodukte" },
+  { value: 164, label: "Obst" },
+  { value: 11, label: "Hülsenfrüchte" },
+  { value: 155, label: "Wurzelgemüse" },
+  { value: 12, label: "Nüsse & Samen" },
+  { value: 44, label: "Pflanzenöle" },
+  { value: 69, label: "Zucker" },
+  { value: 46, label: "Geflügel" },
+  { value: 27, label: "Fisch" },
+  { value: 83, label: "rotes Fleisch" },
+  { value: 31, label: "Eier" },
+  { value: 3, label: "Palmöl" },
+  { value: 21, label: "tierisches Fett" },
 ];
 
 // Startseite laden
